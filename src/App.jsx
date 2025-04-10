f02f3cdb371414eb4cf0537756f07e4d5b411965
kouki0318 <shimadak2003@gmail.com>
2025-04-10 16:58:37 +0900

    App.jsx更新

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import { useState } from 'react';
+
+const API_KEY = 'あなたのAPIキー'; // ← 自分のOpenWeatherMap APIキーをここに入れる！
+
+function App() {
+  const [city, setCity] = useState('');
+  const [weather, setWeather] = useState(null); // 結果表示用
+
+
+  const handleSearch = async () => {
+    if (!city.trim()) return;
+
+
+    try {
+      const res = await fetch(
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`)
+      );
+      const data = await res.json();
+      setWeather(data);
+    } catch (err) {
+      console.error('エラーが発生しました。', err);
+    }
+  };
+
+
+
+  return (
+    <div>
+      <h1>天気アプリ</h1>
+      <input
+       value={city}
+       onChange={(e) => setCity(e.target.value)}
+       placeholder="都市名を入力（例：Tokyo）"
+       />
+      <button onClick={handleSearch}>検索</button>
+
+      {weather && weather.main && (
+        <div>
+          <h2>{weather.name}の天気</h2>
+          <p>🌡️ 温度: {weather.main.temp}℃</p>
+          <p>☁️ 天気: {weather.weather[0].description}</p>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
